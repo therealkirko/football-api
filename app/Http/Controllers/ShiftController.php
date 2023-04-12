f1ffa7e75772006540ce16df7e1ab1d0304df680
John Kiriko <john.muriu95@gmail.com>
2023-04-12 14:34:50 +0300

    Updates

--- app/Http/Controllers/ShiftController.php
+++ app/Http/Controllers/ShiftController.php
@@ -12,15 +12,13 @@ use Illuminate\Support\Facades\Auth;
 
 class ShiftController extends Controller
 {
-    public function checkStatus($instoreId)
+    public function checkStatus($uuid)
     {
         try {
-
-            $today = Carbon::today();
-
+            $user = Ambassador::where('id', Auth::user()->id)->first();
             $shift = Shift::where('ambassador_id', $user->id)
-                ->where('instore_id', $instoreId)
-                ->where('created_at', '>=', $today)
+                ->where('uuid', $uuid)
+                ->whereDate('created_at', Carbon::today())
                 ->first();
 
             if (!$shift){
@@ -57,94 +55,6 @@ class ShiftController extends Controller
         }
     }
 
-    public function checkClockIn($uuid)
-    {
-        $user = Ambassador::where('id', Auth::user()->id)->first();
-
-        $shift = Shift::where('ambassador_id', $user->id)
-            ->where('uuid', $uuid)
-            ->whereDate('created_at', Carbon::today())
-            ->first();
-
-        if (!$shift){
-            return response()->json([
-                'id' => 1,
-                'name' => 'clock in'
-            ], 200);
-        }else {
-            return response()->json([
-                'error' => false,
-                'message' => 'Already clocked in.'
-            ], 200);
-        }
-    }
-
-    public function checkSelfie($uuid)
-    {
-        $user = Ambassador::where('id', Auth::user()->id)->first();
-
-        $shift = Shift::where('ambassador_id', $user->id)
-            ->where('uuid', $uuid)
-            ->whereDate('created_at', Carbon::today())
-            ->first();
-
-        if(!$shift->hasPersonalPhoto) {
-            return response()->json([
-                'id' => 2,
-                'name' => 'selfie',
-            ], 200);
-        }else {
-            return response()->json([
-                'error' => false,
-                'message' => 'Already taken a selfie.',
-            ], 200);
-        }
-    }
-
-    public function checkShelfPhoto($uuid)
-    {
-        $user = Ambassador::where('id', Auth::user()->id)->first();
-
-        $shift = Shift::where('ambassador_id', $user->id)
-            ->where('uuid', $uuid)
-            ->whereDate('created_at', Carbon::today())
-            ->first();
-
-        if(!$shift->hasShelfPhoto) {
-            return response()->json([
-                'id' => 2,
-                'name' => 'shelf',
-            ], 200);
-        }else {
-            return response()->json([
-                'error' => false,
-                'message' => 'Already taken a photo of shelf.',
-            ], 200);
-        }
-    }
-
-    public function checkStockUpdate()
-    {
-        $user = Ambassador::where('id', Auth::user()->id)->first();
-
-        $shift = Shift::where('ambassador_id', $user->id)
-            ->where('uuid', $uuid)
-            ->whereDate('created_at', Carbon::today())
-            ->first();
-
-        if(!$shift->hasUpdatedStock) {
-            return response()->json([
-                'id' => 2,
-                'name' => 'shelf',
-            ], 200);
-        }else {
-            return response()->json([
-                'error' => false,
-                'message' => 'Already updated stock.',
-            ], 200);
-        }
-    }
-
     public function index(Request $request)
     {
         try {
@@ -164,7 +74,7 @@ class ShiftController extends Controller
                 'error' => false,
                 'message' => 'You have successfully started your shift today.'
             ], 200);
-
+            
         } catch (\Exception $e) {
             return response()->json([
                 'error' => true,
@@ -197,7 +107,7 @@ class ShiftController extends Controller
                 'error' => false,
                 'message' => "You have successfully taken today's stock take."
             ], 200);
-
+            
         } catch (\Exception $e) {
             return response()->json([
                 'error' => true,
