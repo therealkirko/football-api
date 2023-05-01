0c97d54ea326f4bb5840ce17fde60a1114a2f7ba
John Kiriko <john.muriu95@gmail.com>
2023-05-01 17:55:15 +0300

    Fixed customer model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -16,4 +16,10 @@ class Product extends Model
     {
         return $this->belongsToMany(Reward::class);
     }
+
+    protected $hidden = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
 }
